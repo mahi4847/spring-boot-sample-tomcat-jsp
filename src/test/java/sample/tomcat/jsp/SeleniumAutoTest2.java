7c4dca25eeecea9a4715626a90b7d20a058a8fe5
mahi4847 <47141279+mahi4847@users.noreply.github.com>
2019-02-12 12:59:30 +0530

    Create SeleniumAutoTest2.java

---
+++ src/test/java/sample/tomcat/jsp/SeleniumAutoTest2.java
@@ -0,0 +1,39 @@
+package sample.tomcat.jsp;
+ 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+ 
+public class SeleniumAutoTest2 {
+ 
+ public WebDriver driver;
+ 
+  @Test
+  public void openMyBlog1() {
+// 	driver.get("http://35.166.134.157:8080/user");
+  }
+  @Test
+  public void testController1() {
+// 	driver.get("http://35.166.134.157:8080/user");
+  }
+ 
+  @Test
+  public void testController2() {
+// 	driver.get("http://35.166.134.157:8080/user");
+  }
+  @BeforeClass
+  public void beforeClass() {
+   
+   //System.setProperty("webdriver.gecko.driver", "D:\\Selenium\\Drivers\\geckodriver.exe");
+  // driver = new FirefoxDriver();
+   
+  }
+ 
+  @AfterClass
+  public void afterClass() {
+  // driver.quit();
+  }
+ 
+}
