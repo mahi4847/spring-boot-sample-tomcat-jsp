53a17ccb09cfe80fb743ae32ebffacc79eb6d4be
Cloud User <centos@ip-10-0-1-119.us-west-2.compute.internal>
2019-02-12 06:32:00 +0000

    updated for test

---
+++ src/test/java/sample/tomcat/jsp/SeleniumAutoTest.java
@@ -0,0 +1,31 @@
+package sample.tomcat.jsp;
+ 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+ 
+public class SeleniumAutoTest {
+ 
+ public WebDriver driver;
+ 
+  @Test
+  public void openMyBlog() {
+// 	driver.get("http://35.166.134.157:8080/user");
+  }
+  
+  @BeforeClass
+  public void beforeClass() {
+   
+   //System.setProperty("webdriver.gecko.driver", "D:\\Selenium\\Drivers\\geckodriver.exe");
+  // driver = new FirefoxDriver();
+   
+  }
+ 
+  @AfterClass
+  public void afterClass() {
+  // driver.quit();
+  }
+ 
+}
